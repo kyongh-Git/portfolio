0de8c96437704691fa13bea241bcc93df7540d31
Yonghwan Kim <kyongh@okstate.edu>
2022-10-09 17:22:35 -0500

    Updates

---
+++ precache-manifest.984cd0ee9e9b5a77bae6df8450316a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a008c5902d75165047575ab06520f1c3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eb7b372d5b973a6476d1",
+    "url": "/portfolio/static/css/main.5745f8f2.chunk.css"
+  },
+  {
+    "revision": "a272dd520dd6cc7f040f",
+    "url": "/portfolio/static/js/2.23104c0a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.23104c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb7b372d5b973a6476d1",
+    "url": "/portfolio/static/js/main.b72bbf2b.chunk.js"
+  },
+  {
+    "revision": "039f2d7365dd35a454e3",
+    "url": "/portfolio/static/js/runtime-main.39439db2.js"
+  }
+]);
